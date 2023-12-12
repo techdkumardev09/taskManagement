fb2cf4116913f3b4a3a5e203d4f8af542f4721ff
techdkumardev09 <techdkumardev0944@gmail.com>
2023-12-12 14:18:08 +0530

    feat: update, delete api

--- Backend/src/controller/taskController.ts
+++ Backend/src/controller/taskController.ts
@@ -4,22 +4,66 @@ const taskController = {
   async createTask(req: Request, res: Response) {
     try {
       const task = await Task.create(req.body);
-      res.status(201).json(task);
+      res.status(201).json({ message: "Task created successfully", task });
     } catch (error) {
       res
         .status(500)
-        .json({ error: "An error occurred while creating the task." });
+        .json({ error: "Failed to create the task. Please try again later." });
     }
   },
   async getAllTasks(req: Request, res: Response) {
     try {
       const tasks = await Task.find();
 
-      res.json(tasks);
+      res.json({ message: "Tasks fetched successfull", tasks });
     } catch (error) {
       res
         .status(500)
-        .json({ error: "An error occurred while fetching tasks." });
+        .json({ error: "Failed to fetch tasks. Please try again later." });
+    }
+  },
+  async getTaskById(req: Request, res: Response) {
+    try {
+      const task = await Task.findById(req.params.id);
+      if (task) {
+        res.json(task);
+      } else {
+        res.status(404).json({ error: "Task not found." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to fetch the task. Please try again later." });
+    }
+  },
+  async deleteTask(req: Request, res: Response) {
+    try {
+      const task = await Task.findByIdAndDelete(req.params.id);
+      if (task) {
+        res.json({ message: "Task deleted successfully !" });
+      } else {
+        res.status(404).json({ error: "Task not found." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to delete the task. Please try again later." });
+    }
+  },
+  async updateTask(req: Request, res: Response) {
+    try {
+      const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      if (task) {
+        res.json({ message: "Task updated successfully", task });
+      } else {
+        res.status(404).json({ error: "Task not found." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to update the task. Please try again later." });
     }
   },
 };
--- Backend/src/routes/taskRoutes.ts
+++ Backend/src/routes/taskRoutes.ts
@@ -4,5 +4,8 @@ import taskController from "../controller/taskController";
 const taskRoutes = express.Router();
 taskRoutes.post("/", taskController.createTask);
 taskRoutes.get("/", taskController.getAllTasks);
+taskRoutes.get("/:id", taskController.getTaskById);
+taskRoutes.delete("/:id", taskController.deleteTask);
+taskRoutes.put("/:id", taskController.updateTask);
 
 export default taskRoutes;
