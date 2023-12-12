461a4a45ec6babdeed0b962a797bb58a727fcc2d
techdkumardev09 <techdkumardev0944@gmail.com>
2023-12-12 18:40:41 +0530

    feat: add 404 pages

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { allImages, buttonType } from '../utils/constants';
+import { useNavigate } from 'react-router-dom';
+import Heading from '../components/Heading';
+import Button from "../components/Button/Button";
 
 const NotFound = () => {
-  return (
-    <div>
-      NotFound
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default NotFound
+  return (
+    <>
+      <div className="flex justify-center mt-5">
+        <img src={allImages.imageNotFound} width={"40%"} alt="not found" />
+      </div>
+      <div className="flex flex-col justify-center items-center mt-5">
+        <Heading title="Something went wrong" className="text-center" />
+        <Button
+          onClick={() => navigate("/")}
+          className={`${buttonType.primary} mt-6`}
+        >
+          Go to Home
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
